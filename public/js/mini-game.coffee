a532b00465a0c75d3cdf8da71bd169e9c668daa5
jschomay <jeffpublic@gmail.com>
2013-04-15 17:39:24 -0700

    wip, interactive elements built into animation sequence

--- public/js/mini-game.coffee
+++ public/js/mini-game.coffee
@@ -15,33 +15,67 @@
       console.log "game started ", getTime()
 
       @$stage = $ '#about'
+      @currentImageFragmentNumber= 1
+      @remainingKeys = @keys
 
-      # kick off animation sequence after delay
-      # run callback function when done
+      # kick off animation sequence after 1s...
       @delay 1000, =>
-        cb = -> console.log 'DONE'
-        @animate @sequence1, 0, cb
+        @runSequence @introSequence, 0, =>
+          # then run renderImageFragment callback when done (wait 6s first)...
+          @delay 6000, @renderImageFragment =>
+            # then run sequence2 after 1s
+            @delay 1000, =>
+              @runSequence @sequence2, 0
+              # then do nothing (wait for click event)
+
+
+    # queues up the proper image and keys and renders them
+    renderImageFragment: (cb) =>
+      f = @assets[@currentImageFragmentNumber-1]
+
+      output = [
+        s: '<br/><br/><span style="color:green;">[LOADING IMAGE FRAGMENT '+@currentImageFragmentNumber+']</span>'
+      ,
+        d: 1000, s: '<br/><img class="fragment" src="'+f.src+'" border="1" alt="'+f.key+'"/><br/>Asset keys:'
+      ]
+
+      # add keys to output
+      # jquery objects, prebound with click event
+      for key in @remainingKeys
+        k = $ '<span class="key" id="'+key+'">["'+key+'"]</span>'
+        k.on 'click', (e) => @checkKey e.target.id
+        output.push 
+          d: 0
+          s: [' ', k]
+
+      @runSequence output, 0, cb
 
     # run render function on each step in the animation sequence,
     # and call the next step in line with it's dely
-    animate: (sequence, step, cb) =>
+    runSequence: (sequence, step, cb) =>
       @render sequence[step].s
       if step < sequence.length - 1
         step++
         @delay sequence[step].d, =>
-          @animate sequence, step, cb
+          @runSequence sequence, step, cb
       else
-        cb()
+        cb?()
+
+
+    # render() is just a proxy, so you could switch out render functions
+    render: (data) =>
+      # defaults to @linefeed
+      @linefeed data
 
     # this render function just appends the current string to the 
     # end of the page and scrolls to the bottom if needed
-    render: (data) ->
+    linefeed: (data) ->
       @$stage.append data
       # scroll page to bottom
       $(window).scrollTop($(document).height())
 
     # first animation sequence with delay and string
-    sequence1: [
+    introSequence: [
       d: 200, s: '<br/>' + new Date() + ' - <span style="color: red;">ERROR (42): site interrupt - User pressed the wrong button</span>'
     ,
       d: 200, s: '<br/>&nbsp; &nbsp; &nbsp; &nbsp; Button id: #dont-push (index.html:32:17)'
@@ -81,15 +115,43 @@
       d: 3000, s: '<br/><span style="color:yellow;"> &nbsp;' + getTime() + 'jschomay: </span>I <em>knew </em>you were going to press that button!!'
     ,
       d: 3500, s: '<br/><span style="color:yellow;"> &nbsp;' + getTime() + 'jschomay: </span>It\'s ok, we can fix this.  I just need a little help from you to reconnect my image assets to their database keys.  Think of it as a game.  Just follow along.  One second...'
-    ,
-      d: 6000, s: '<br/><br/><span style="color:green;">[LOADING IMAGE FRAGMENT 1]</span>'
-    ,
-      d: 1000, s: '<br/><img with="100" height="100" src="" border="1" alt="img here"/><br/>Asset keys: <span style="color:gray;">["origami"], ["juggler"], ["eagle_scout"], ["unicycle"], ["travel"]</span>'
-    ,
+    ]
+
+    # plays afterfirst image fragment is displayed
+    sequence2:[
       d: 2000, s: '<br/><br/><span style="color:yellow;"> &nbsp;' + getTime() + 'jschomay - </span>Ok, it\'s your turn.  Click on the asset key you think this image fragment fits with.'
     ,
       d: 400, s: '<br/><br/>Waiting for user input...'
-    ] 
+    ]
+
+    # images used in matching
+    assets: [
+      src: 'images/unicycle.jpg'
+      key: 'uniycle'
+      info: 'Yup, I ride a 6 foot tall unicycle for fun and professionally :)'
+    ,
+      src: 'images/juggler.jpg'
+      key: 'juggler'
+      info: 'I was a professional juggler for many years'
+    ,
+      src: 'images/eagle_scout.jpg'
+      key: 'eagle_scout'
+      info: 'Many people don\'t know I\'m an Eagle Scout.'
+    ,
+      src: 'images/origami.jpg'
+      key: 'origami'
+      info: 'I\'ve made origami since I was 7 years old'
+    ,
+      src: 'images/travel.jpg'
+      key: 'travel'
+      info: 'I lived in Europe for 5 years when I was young, and have traveled around the world'
+    ]
+
+    # asset keys
+    keys: [
+      "origami", "juggler", "scuba_dive", "eagle_scout", "unicycle", "travel", "sky_diving"
+    ]
+    # a.splice(a.indexOf('four'),1)
 
 
     # run the passed function after a delay
--- public/js/mini-game.js
+++ public/js/mini-game.js
@@ -16,7 +16,9 @@ var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments)
   };
   Game = (function() {
     function Game() {
-      this.animate = __bind(this.animate, this);
+      this.render = __bind(this.render, this);
+      this.runSequence = __bind(this.runSequence, this);
+      this.renderImageFragment = __bind(this.renderImageFragment, this);
     }
 
     Game.prototype.start = function() {
@@ -24,36 +26,77 @@ var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments)
 
       console.log("game started ", getTime());
       this.$stage = $('#about');
+      this.currentImageFragmentNumber = 1;
+      this.remainingKeys = this.keys;
       return this.delay(1000, function() {
-        var cb;
-
-        cb = function() {
-          return console.log('DONE');
-        };
-        return _this.animate(_this.sequence1, 0, cb);
+        return _this.runSequence(_this.introSequence, 0, function() {
+          return _this.delay(6000, _this.renderImageFragment(function() {
+            return _this.delay(1000, function() {
+              return _this.runSequence(_this.sequence2, 0);
+            });
+          }));
+        });
       });
     };
 
-    Game.prototype.animate = function(sequence, step, cb) {
+    Game.prototype.renderImageFragment = function(cb) {
+      var f, k, key, keys, output, _i, _j, _len, _len1, _ref, _ref1;
+
+      f = this.assets[this.currentImageFragmentNumber - 1];
+      keys = '';
+      _ref = this.remainingKeys;
+      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+        key = _ref[_i];
+        keys += '<span class="key" id="' + key + '">["' + key + '"]</span>, ';
+      }
+      keys = keys.slice(0, -2);
+      output = [
+        {
+          s: '<br/><br/><span style="color:green;">[LOADING IMAGE FRAGMENT ' + this.currentImageFragmentNumber + ']</span>'
+        }, {
+          d: 1000,
+          s: '<br/><img class="fragment" src="' + f.src + '" border="1" alt="' + f.key + '"/><br/>Asset keys:'
+        }
+      ];
+      _ref1 = this.remainingKeys;
+      for (_j = 0, _len1 = _ref1.length; _j < _len1; _j++) {
+        key = _ref1[_j];
+        k = $('<span class="key" id="' + key + '">["' + key + '"]</span>');
+        k.on('click', function(e) {
+          return console.log(e.target.id);
+        });
+        output.push({
+          d: 0,
+          s: [' ', k]
+        });
+      }
+      return this.runSequence(output, 0, cb);
+    };
+
+    Game.prototype.runSequence = function(sequence, step, cb) {
       var _this = this;
 
       this.render(sequence[step].s);
       if (step < sequence.length - 1) {
         step++;
         return this.delay(sequence[step].d, function() {
-          return _this.animate(sequence, step, cb);
+          return _this.runSequence(sequence, step, cb);
         });
       } else {
-        return cb();
+        return typeof cb === "function" ? cb() : void 0;
       }
     };
 
     Game.prototype.render = function(data) {
+      return this.linefeed(data);
+    };
+
+    Game.prototype.linefeed = function(data) {
       this.$stage.append(data);
       return $(window).scrollTop($(document).height());
     };
 
-    Game.prototype.sequence1 = [
+    Game.prototype.introSequence = [
       {
         d: 200,
         s: '<br/>' + new Date() + ' - <span style="color: red;">ERROR (42): site interrupt - User pressed the wrong button</span>'
@@ -114,13 +157,11 @@ var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments)
       }, {
         d: 3500,
         s: '<br/><span style="color:yellow;"> &nbsp;' + getTime() + 'jschomay: </span>It\'s ok, we can fix this.  I just need a little help from you to reconnect my image assets to their database keys.  Think of it as a game.  Just follow along.  One second...'
-      }, {
-        d: 6000,
-        s: '<br/><br/><span style="color:green;">[LOADING IMAGE FRAGMENT 1]</span>'
-      }, {
-        d: 1000,
-        s: '<br/><img with="100" height="100" src="" border="1" alt="img here"/><br/>Asset keys: <span style="color:gray;">["origami"], ["juggler"], ["eagle_scout"], ["unicycle"], ["travel"]</span>'
-      }, {
+      }
+    ];
+
+    Game.prototype.sequence2 = [
+      {
         d: 2000,
         s: '<br/><br/><span style="color:yellow;"> &nbsp;' + getTime() + 'jschomay - </span>Ok, it\'s your turn.  Click on the asset key you think this image fragment fits with.'
       }, {
@@ -128,6 +169,32 @@ var __bind = function(fn, me){ return function(){ return fn.apply(me, arguments)
         s: '<br/><br/>Waiting for user input...'
       }
     ];
+
+    Game.prototype.assets = [
+      {
+        src: 'images/unicycle.jpg',
+        key: 'uniycle',
+        info: 'Yup, I ride a 6 foot tall unicycle for fun and professionally :)'
+      }, {
+        src: 'images/juggler.jpg',
+        key: 'juggler',
+        info: 'I was a professional juggler for many years'
+      }, {
+        src: 'images/eagle_scout.jpg',
+        key: 'eagle_scout',
+        info: 'Many people don\'t know I\'m an Eagle Scout.'
+      }, {
+        src: 'images/origami.jpg',
+        key: 'origami',
+        info: 'I\'ve made origami since I was 7 years old'
+      }, {
+        src: 'images/travel.jpg',
+        key: 'travel',
+        info: 'I lived in Europe for 5 years when I was young, and have traveled around the world'
+      }
+    ];
+
+    Game.prototype.keys = ["origami", "juggler", "scuba_dive", "eagle_scout", "unicycle", "travel", "sky_diving"];
 
     Game.prototype.delay = function(delay, fn) {
       return setTimeout(fn, delay);
