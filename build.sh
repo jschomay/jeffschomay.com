afc2272b86c463dbb0630c4220c4ea8b568c6f39
Jeff Schomay <jschomay@gmail.com>
2014-08-17 12:09:55 +0100

    update link to panoramic photo site
    moved it to photography.jeffschomay.com
    
    also updated build script for new version of jade

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # compile views/index.jade to public/index.html
-./node_modules/jade/bin/jade -P -O public/ views/index.jade 
+./node_modules/jade/bin/jade.js -P -o public/ views/index.jade
 
 # todo?
 # compile and minify coffee scripts and stylus
